cbe3c3eea503e62803a483a919e1fc7805690f18
a2g-leo <142281343+a2g-leo@users.noreply.github.com>
2023-09-20 11:13:30 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+class Config(object):
+    # Get a bot token from @botfather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6578868504:AAEtWfmcbtsWV6xjPGuVe29LD1oHRUSW6gc")
+
+    # Get from my.telegram.org
+    APP_ID = int(os.environ.get("APP_ID", "29310987"))
+
+    # Get from my.telegram.org
+    API_HASH = os.environ.get("API_HASH", "336c28913a45587a4c10af8cd620b68f")
+
+    # Authorized users to use this bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6515901556").split())
+
+    # session name
+    TG_USER_SESSION_NAME = os.environ.get("TG_USER_SESSION_NAME", "oxmohsen")
+
+    # tg user session string
+    TG_USER_SESSION_STRING = os.environ.get("TG_USER_SESSION_STRING", "")
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
